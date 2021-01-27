d91e12298ea80bd6f7d7687260f6360de322f333
rohit <rohitsdec4@gmail.com>
2021-01-27 12:00:39 +0530

    Removed useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import './App.css';
 import Header from './components/HeaderComponent';
 import MainComponent from "./components/MainComponent";
